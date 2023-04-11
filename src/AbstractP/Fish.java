a08993dd065a051bdb453394d045259d985ca150
Jainesh Captain <jaineshcaptain21@gmail.com>
2023-04-11 01:34:12 -0400

    Abstraction

---
+++ src/AbstractP/Fish.java
@@ -0,0 +1,27 @@
+package AbstractP;
+
+public class Fish extends Animal {
+
+    public Fish(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+        if (speed.equals("slow")) {
+            System.out.println(type + " lazily swimming");
+        } else {
+            System.out.println(type + " darting");
+        }
+
+    }
+
+    @Override
+    public void makeNoise() {
+        if (type == "Goldfish") {
+            System.out.print("swish");
+        } else {
+            System.out.print("splash ");
+        }
+    }
+}
---
+++ src/AbstractP/Horse.java
@@ -0,0 +1,19 @@
+package AbstractP;
+
+public class Horse extends Mammal {
+
+    public Horse(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println(getExplicitType() + "sheds in the spring");
+    }
+
+    @Override
+    public void shedHair() {
+
+    }
+
+}
